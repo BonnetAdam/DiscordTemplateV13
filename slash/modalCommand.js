c3a9795b36cf47aeafbd528494f197f9228346c9
CrazyOutOff <dadouneprank@gmail.com>
2022-03-15 21:31:34 +0100

    Add Working ModalExampleSystem

--- slash/modalCommand.js
+++ slash/modalCommand.js
@@ -2,29 +2,21 @@ const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports.execute = async (interaction, client, lang) => {
     const row = new MessageActionRow()
-    .addComponents(
-        new MessageButton()
-        .setCustomId('modal')
-        .setLabel('Primary')
-        .setStyle('PRIMARY'),
-        );
-        await interaction.reply({content: lang.command.everythingWork, components: [row], ephemeral: true});
+        .addComponents(
+            new MessageButton()
+            .setCustomId('modal')
+            .setLabel('Modal')
+            .setStyle('PRIMARY'),
+            );
+    await interaction.reply({content: lang.command.everythingWork, components: [row], ephemeral: true});
 }
 
 //              Slash Commands
 module.exports.help = {
-    name: "test", //Name of the command
-    description: "test command", //Description of the command
+    name: "modal", //Name of the command
+    description: "Test the modal system", //Description of the command
     type: 1, //Type of the command. Read the readme.md file for more information
-    options: [
-        {
-            type: 3, //String
-            name: "test1", //Option Name
-            description: "description for the test1 arguments", //Option description
-            required: false //Does the value need to be put in
-        },
-    ], //Option of the command
-    permission: "ADMINISTRATOR", //Required permission to use the command
+    permission: "SEND_MESSAGES", //Required permission to use the command
     enable: true, //If the command is under maintenance mode
     show: true //If the command is show on the help menu
 };
